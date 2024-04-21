b63c93f088f3a9c160d879cf88abad29bed1007f
salik03 <uddinsalik03@gmail.com>
2024-04-21 13:29:58 +0530

    resume builder added

---
+++ atsresume/components/utility/DefaultResumeData.jsx
@@ -0,0 +1,93 @@
+const DefaultResumeData = {
+  name: "SALIK UDDIN",
+  position: "Backend Developer",
+  contactInformation: "+91-1234567809",
+  email: "uddinsalik@outlook.com",
+  address: "Lucknow, India",
+  profilePicture: "",
+  socialMedia: [
+    {
+      socialMedia: "Github",
+      link: "github.com//salik03",
+    },
+    {
+      socialMedia: "LinkedIn",
+      link: "linkedin.com/in/salik-uddin",
+    },
+    {
+      socialMedia: "Website",
+      link: "salik.live",
+    },
+  ],
+  summary: "An ambitious and aspiring to be software engineer with skills in coding, testing, code reviews, code comments, and other software engineering processes. Strong client communication skills, as well as the ability to articulate ideas simply and succinctly. I feel I possess a great deal of potential and versatility, as well as the ability to adapt and learn quickly.",
+  education: [
+    {
+      "school": "Bachelor of Technology: Computer Science",
+      "degree": "Bennett University",
+      "startYear": "2021-08-20",
+      "endYear": "2025-07-01"
+    },
+  ],
+  "workExperience": [
+    {
+      "company": "Freelance Remote",
+      "position": "Flutter App Developer",
+      "description": "Crafted custom apps tailored to clients' requirements using the Flutter framework.",
+      "keyAchievements": "Solved complex technical challenges.\nEnsured on-time project completion.\nManaged client relations maintaining strong communication and rapport.\nIntegrated advanced visual and functional elements that enhanced user engagement.\nStreamlined application performance which led to a 20% increase in user retention.",
+      "startYear": "2023-06",
+      "endYear": "2023-08"
+    },
+    {
+      "company": "Freelance Remote",
+      "position": "React Native App Developer",
+      "description": "Built mobile applications that met client needs using React Native.",
+      "keyAchievements": "Addressed challenging technical issues.\nManaged client communications effectively.\nEnsured projects were completed on schedule.\nOptimized application code which improved performance by 15%.\nDeveloped reusable components that reduced development time for future projects.",
+      "startYear": "2023-07",
+      "endYear": "2023-08"
+    },
+    {
+      "company": "Spill Your Thoughts",
+      "position": "Technical Writing Intern",
+      "description": "Internship focusing on technical writing and software development.",
+      "keyAchievements": "Developed user guides and technical documents.\nCreated a cross-platform application to alert users about potential spam content.\nEnhanced problem-solving skills.\nContributed to the creation of a knowledge base for internal use that improved project onboarding time.\nParticipated in various team projects that increased the company's output by 10%",
+      "startYear": "2022-11",
+      "endYear": "2023-1"
+    }
+  ],
+    "projects": [],
+    "skills": [
+      {
+        "title": "Technical Skills",
+        "skills": [
+          "Flutter", "React Native", "Kotlin", "Java", "Dart", "Cloud", "Swift", "Objective C", "C#", "C++", "Unity", "Unreal Engine", "Prisma", "Node.js", "TypeScript", "Python | Streamlit", "JavaScript", "Tailwind CSS", "Flask", "Figma"
+        ]
+      },
+      {
+        "title": "Soft Skills",
+        "skills": [
+          "Collaboration", "Problem-solving", "Communication", "Time management", "Result-oriented"
+        ]
+      },
+      {
+        "title": "Additional Skills",
+        "skills": [
+          "Public Speaking", "Writing", "Research"
+        ] 
+      }
+    ],
+    "languages": [
+      "English",
+      "Hindi"
+    ],
+    "certifications": [
+      "Certified Web Professional - Web Developer",
+      "Java Development Certified Professional",
+      "AWS Academy Cloud Architecting",
+      "AWS Academy Cloud Foundations",
+      "Foundations of Project Management, Google",
+      "The Bits and Bytes of Computer Networking",
+      "Software Engineering: Modeling Software Systems using UML"
+    ],
+};
+
+export default DefaultResumeData;
