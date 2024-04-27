7470deb06a34225ceebfaae4cbe60578b476d98d
salik03 <uddinsalik03@gmail.com>
2024-04-28 05:01:55 +0530

    you dont need this stuff

--- frontend/src/_mock/account.js
+++
@@ -1,7 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const account = {
-  displayName: 'Salik Uddin',
-  email: 'uddinsalik@outlook.com',
-  photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCLRMVRxKocFBEW-hM-UAeSobWCuJSeC2o5idGx9Dnqg&s',
-};
