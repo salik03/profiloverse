59f15d9b449337e5e24bfc09b484411f5135aaa5
Mansi Chaudhary <mansi@Mansis-MacBook-Air.local>
2024-04-30 02:39:03 +0530

    pushing some changes

--- frontend/src/sections/overview/view/app-view.jsx
+++ frontend/src/sections/overview/view/app-view.jsx
@@ -14,6 +14,8 @@ import AppWidgetSummary from '../app-widget-summary';
 import AppTrafficBySite from '../app-traffic-by-site';
 import AppCurrentSubject from '../app-current-subject';
 import AppConversionRates from '../app-conversion-rates';
+import AppTasks from '../app-tasks';
+// import AppTasks from '/Users/mansi/Desktop/profiloverse/frontend/src/sections/overview/app-tasks.jsx';
 
 // ----------------------------------------------------------------------
 
@@ -27,16 +29,16 @@ export default function AppView() {
       <Grid container spacing={3}>
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="Weekly Sales"
-            total={714000}
+            title="Companies Hiring"
+            total={42000}
             color="success"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_companies.png" />}
           />
         </Grid>
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="New Users"
+            title="Active Job Seekers"
             total={1352831}
             color="info"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
@@ -45,123 +47,105 @@ export default function AppView() {
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="Item Orders"
-            total={1723315}
+            title="Opportunities Posted"
+            total={700000}
             color="warning"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_opportunities.png" />}
           />
         </Grid>
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
+            title="Students Employed"
+            total={488000}
             color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_employed.png" />}
           />
         </Grid>
 
-        <Grid xs={12} md={6} lg={8}>
+        {/* <Grid xs={12} md={6} lg={8}>
           <AppWebsiteVisits
-            title="Website Visits"
+            title="Company Hiring Trend"
             subheader="(+43%) than last year"
             chart={{
               labels: [
-                '01/01/2003',
-                '02/01/2003',
-                '03/01/2003',
-                '04/01/2003',
-                '05/01/2003',
-                '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
+                '01/01/2013',
+                '01/01/2014',
+                '01/01/2015',
+                '01/01/2016',
+                '01/01/2017',
+                '01/01/2018',
+                '01/01/2019',
+                '01/01/2020',
+                '01/01/2021',
+                '01/01/2022',
+                '01/01/2023',
               ],
               series: [
                 {
-                  name: 'Team A',
+                  name: 'Google',
                   type: 'column',
                   fill: 'solid',
                   data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
                 },
                 {
-                  name: 'Team B',
+                  name: 'Apple',
                   type: 'area',
                   fill: 'gradient',
                   data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
                 },
                 {
-                  name: 'Team C',
+                  name: 'Microsoft',
                   type: 'line',
                   fill: 'solid',
                   data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
                 },
+                {
+                  name: 'Meta',
+                  type: 'line',
+                  fill: 'solid',
+                  data: [36, 30, 45, 30, 49, 52, 59, 51, 46, 39, 35],
+                },
               ],
             }}
           />
-        </Grid>
+        </Grid> */}
 
-        <Grid xs={12} md={6} lg={4}>
+        {/* <Grid xs={12} md={6} lg={4}>
           <AppCurrentVisits
-            title="Current Visits"
+            title="Skills in demand"
             chart={{
               series: [
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
+                { label: 'Project Management', value: 4044 },
+                { label: 'Generative AI', value: 5435 },
+                { label: 'Machine Learning', value: 3443 },
+                { label: 'Communication', value: 4643 },
+                { label: 'Cybersecurity', value: 2857 },
+                { label: 'Sustainability', value: 1832 },
+                // { label: 'Computing', value: 1409 },
               ],
             }}
           />
-        </Grid>
+        </Grid> */}
 
         <Grid xs={12} md={6} lg={8}>
           <AppConversionRates
-            title="Conversion Rates"
-            subheader="(+43%) than last year"
+            title="Employee Hiring Rate"
+            subheader="(+33%) than last year"
             chart={{
               series: [
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
+                { label: 'Apple', value: 400 },
+                { label: 'Meta', value: 460 },
+                { label: 'Scaler', value: 548 },
+                { label: 'Adobe', value: 690 },
+                { label: 'Amazon', value: 810 },
+                { label: 'Google', value: 900 },
+                { label: 'Microsoft', value: 950 },
+                { label: 'Amazon', value: 1100 },
+                { label: 'Deloitte', value: 1200 },
+                { label: 'TCS', value: 1380 },
               ],
             }}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppCurrentSubject
-            title="Current Subject"
-            chart={{
-              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
-              series: [
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ],
-            }}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <AppNewsUpdate
-            title="Recent Job Updates"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: faker.person.jobTitle(),
-              description: faker.company.name(),
-              image: `/assets/images/covers/cover_${index + 1}.jpg`,
-              postedAt: faker.date.recent(),
-            }))}
           />
         </Grid>
 
@@ -183,7 +167,69 @@ export default function AppView() {
           />
         </Grid>
 
+        {/* <Grid xs={12} md={6} lg={4}>
+          <AppCurrentSubject
+            title="Current Subject"
+            chart={{
+              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
+              series: [
+                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
+                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
+                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
+              ],
+            }}
+          />
+        </Grid> */}
+
+        <Grid xs={12} md={6} lg={8}>
+          <AppNewsUpdate
+            title="Recent Job Updates"
+            list={[...Array(5)].map((_, index) => ({
+              id: faker.string.uuid(),
+              title: faker.person.jobTitle(),
+              description: faker.company.name(),
+              image: `/assets/images/covers/cover_${index + 1}.jpg`,
+              postedAt: faker.date.recent(),
+            }))}
+          />
+        </Grid>
+
         <Grid xs={12} md={6} lg={4}>
+          <AppCurrentVisits
+            title="Skills in demand"
+            chart={{
+              series: [
+                { label: 'Project Management', value: 4044 },
+                { label: 'Generative AI', value: 5435 },
+                { label: 'Machine Learning', value: 3443 },
+                { label: 'Communication', value: 4643 },
+                { label: 'Cybersecurity', value: 2857 },
+                { label: 'Sustainability', value: 1832 },
+                // { label: 'Computing', value: 1409 },
+              ],
+            }}
+          />
+        </Grid>
+
+        {/* <Grid xs={12} md={6} lg={4}>
+          <AppOrderTimeline
+            title="Internship Timeline"
+            list={[...Array(5)].map((_, index) => ({
+              id: faker.string.uuid(),
+              title: [
+                'Applied at Microsoft for Software Developer Position',
+                'Resume has been noticed',
+                'Resume in review',
+                'Interview to be scheduled',
+                'Confirmation Pending',
+              ][index],
+              type: `order${index + 1}`,
+              time: faker.date.past(),
+            }))}
+          />
+        </Grid> */}
+
+        {/* <Grid xs={12} md={6} lg={4}>
           <AppTrafficBySite
             title="Hiring Trends"
             list={[
@@ -204,13 +250,25 @@ export default function AppView() {
               },
               {
                 name: 'Twitter',
-                value: 443232,
+                value: 443232
                 icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
               },
             ]}
           />
-        </Grid>
+        </Grid> */}
 
+        <Grid xs={12} md={6} lg={8}>
+          <AppTasks
+            title="Tasks"
+            list={[
+              { id: '1', name: 'Assignment due for YardStick.' },
+              { id: '2', name: 'Assignment due for APLTech.' },
+              { id: '3', name: 'Complete your application for SteelEye.' },
+              // { id: '4', name: 'Continuously improve services based on user feedback.' },
+              // { id: '5', name: 'Enhance team expertise and performance as needed.' },
+            ]}
+          />
+        </Grid>
       </Grid>
     </Container>
   );
