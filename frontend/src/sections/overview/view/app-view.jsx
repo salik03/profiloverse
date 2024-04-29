cb23943768e58c1f6687f278db42a6974cddedd1
salik03 <uddinsalik03@gmail.com>
2024-04-30 02:47:39 +0530

    mansi's code merged dashboard changed

--- frontend/src/sections/overview/view/app-view.jsx
+++ frontend/src/sections/overview/view/app-view.jsx
@@ -4,17 +4,12 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
 
-import Iconify from 'src/components/iconify';
-
+import AppTasks from '../app-tasks';
 import AppNewsUpdate from '../app-news-update';
 import AppOrderTimeline from '../app-order-timeline';
 import AppCurrentVisits from '../app-current-visits';
-import AppWebsiteVisits from '../app-website-visits';
 import AppWidgetSummary from '../app-widget-summary';
-import AppTrafficBySite from '../app-traffic-by-site';
-import AppCurrentSubject from '../app-current-subject';
 import AppConversionRates from '../app-conversion-rates';
-import AppTasks from '../app-tasks';
 // import AppTasks from '/Users/mansi/Desktop/profiloverse/frontend/src/sections/overview/app-tasks.jsx';
 
 // ----------------------------------------------------------------------
