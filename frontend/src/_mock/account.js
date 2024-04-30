f4308bcc77bd2da0aee094a2ca420b73152ba53d
salik03 <uddinsalik03@gmail.com>
2024-04-30 19:03:00 +0530

    dp updated successffully

--- frontend/src/_mock/account.js
+++ frontend/src/_mock/account.js
@@ -3,5 +3,5 @@
 export const account = {
   displayName: 'Salik Uddin',
   email: 'uddinsalik@outlook.com',
-  photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCLRMVRxKocFBEW-hM-UAeSobWCuJSeC2o5idGx9Dnqg&s',
+  photoURL: 'https://media.licdn.com/dms/image/D4D03AQGGkRzLcBFkYA/profile-displayphoto-shrink_200_200/0/1697186068142?e=1720051200&v=beta&t=OrVGWXqBgqPwnpNuCRqEKBlvkAvxry_vB4CN0IxTe3Y',
 };
